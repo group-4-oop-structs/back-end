237785d5869b6179d824fb07b7ef4f7bc34d9188
rthec <rthec@MSI>
2016-11-20 15:22:14 -0500

    Added classes for all functionality: sin, cos, tan, sec, csc, cot, arcsin, arccos, arctan, arcsec, arccsc, arccot, ln, log

---
+++ DataStructureElements/Arctan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataStructureElements;
+
+/**
+ *
+ * @author rthec
+ */
+public class Arctan extends UnaryExpression{
+    Expression e;
+
+    public Arctan(Expression e) {
+        this.e = e;
+    }
+
+    public Expression getExpression() {
+        return e;
+    }
+
+    @Override
+    public Expression getDerivative() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
